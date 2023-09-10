4a9f16da19218efa3a33d3fc92c2336a4b037769
ieeemma <emma.freeman@protonmail.com>
2023-09-10 15:14:35 +0100

    feat: file serving

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import path from "path";
+
+Bun.serve({
+  async fetch(req: Request): Promise<Response> {
+    const url = new URL(req.url);
+    const p = path.join("content", path.normalize(url.pathname));
+    switch (req.method) {
+      case "GET":
+        return new Response(Bun.file(p));
+      case "PUT":
+        await Bun.write(p, await req.blob());
+        return new Response(null);
+      default:
+        return new Response("405 Method Not Allowed", {
+          status: 405,
+          statusText: "Method Not Allowed",
+        });
+    }
+  },
+});
